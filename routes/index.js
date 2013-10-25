4ace80808a619a88e04087fea6bc2a31a81a7a82
joshsprague <jjspragu@asu.edu>
2013-10-24 23:45:33 -0700

    Updated index.js with call to Pearson API

--- routes/index.js
+++ routes/index.js
@@ -26,3 +26,25 @@ exports.addTranslation = function(Translation) {
     });
   };
 };
+
+var getTranslation = function(){
+  $.ajax({
+    url: 'http://api.pearson.com/v2/dictionaries/ldec/entries?headword='+attrs.title+'&apikey=d0Q5fQJA1TLjuY8pGYliGnbKWGmnAy8V',
+    dataType: 'jsonp',
+    success: function (body) {
+      if (body["results"][0] === undefined) {
+        //TODO Message user that word is not in dictionary
+      } else if (body["results"][0]["senses"][0]["translation"] === undefined) {
+          newPair.translation = body["results"][0]["senses"][0]["subsenses"][0]["translation"];
+          console.log(newPair.translation);
+      } else {
+          newPair.translation = body["results"][0]["senses"][0]["translation"];
+          console.log(newPair.translation);
+      }
+
+      self.collection.add({title: newPair.search_word, translation: newPair.translation});
+
+      newPair.save({search_word: newPair.search_word, translation: newPair.translation});
+    }
+  });
+};
